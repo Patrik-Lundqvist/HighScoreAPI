c8b0f2077f1de9d65514b6f25e55950819434bc1
Patrik-Lundqvist <pat.lundqvist@gmail.com>
2014-04-16 18:59:40 +0200

    Added empty config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database information
+$databseSettings = array(
+    'driver' => 'pgsql',
+    'host' => '',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix' => ''
+);
+
+// Secret cookie key
+$cookieSecret = "CHANGE_THIS";
